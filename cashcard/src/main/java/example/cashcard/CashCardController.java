05a0b456ea1746c6cf310e1b0801bbdffcff3a9b
premkumar bhaskal <pbhaskal@gmail.com>
2024-05-17 15:06:58 +0530

    added pagination with sorting

--- cashcard/src/main/java/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,9 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,5 +39,17 @@ public class CashCardController {
                 .buildAndExpand(savedCard.id())
                 .toUri();
         return ResponseEntity.created(newCardLocation).build();
+    }
+
+    @GetMapping
+    private ResponseEntity<Iterable<CashCard>> getList(Pageable pageable) {
+        Page<CashCard> page =  cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                )
+        );
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -2,18 +2,25 @@ package example.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 
 	@Autowired
@@ -45,8 +52,9 @@ class CashCardApplicationTests {
 	}
 
 	@Test
+	@DirtiesContext
 	void shouldCreateNewCashCard() {
-		CashCard newCashCard = new CashCard(44L, 250.00);
+		CashCard newCashCard = new CashCard(null, 250.00);
 		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
 		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
@@ -62,4 +70,46 @@ class CashCardApplicationTests {
 		assertThat(newAmount).isEqualTo(250.00);
 	}
 
+	@Test
+	void shouldReturnAllCashCardsWhenListRequested() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		// make sure to add DirtiesContext annotation else items added in another test can reflect here based on test
+//		execution order
+		DocumentContext docContext = JsonPath.parse(response.getBody());
+		int length = docContext.read("$.length()");
+		assertThat(length).isEqualTo(3);
+
+		JSONArray ids = docContext.read("$..id");
+		JSONArray amounts = docContext.read("$..amount");
+		assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+		assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.0, 150.0);
+	}
+
+	@Test
+	void shouldReturnAPageOfCashCards() {
+		ResponseEntity<String> response  = restTemplate.getForEntity("/cashcards?page=0&size=1&sort=amount,desc", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext docContext = JsonPath.parse(response.getBody());
+		JSONArray page = docContext.read("$[*]");
+		assertThat(page.size()).isEqualTo(1);
+
+		double amount = docContext.read("$[0].amount");
+		assertThat(amount).isEqualTo(150.00);
+	}
+
+	@Test
+	void shouldReturnsOfCashCardsWithNoParametersAndUseDefaultValues() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext docContext = JsonPath.parse(response.getBody());
+		JSONArray page = docContext.read("$[*]");
+		assertThat(page.size()).isEqualTo(3);
+
+		JSONArray amounts = docContext.read("$..amount");
+		assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
+	}
 }
